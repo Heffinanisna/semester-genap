4bab8dd51b9786de584c43dfbb3983948d86165b
Heffinanisnaniardini <140322368+Heffinanisna@users.noreply.github.com>
2024-01-19 11:27:04 +0700

    menampilkan tulisan dan HTML

---
+++ tugas-php/tug1.php
@@ -0,0 +1,16 @@
+<?php
+
+    echo "belajar PHP";
+
+    echo '<br>';
+
+    echo 'saya siswa SMK';
+
+    echo '<h1 style ="background-color:blue">belajar PHP itu mudah</h1>
+        <p style ="background-color:yellow">saya suka belajar progam</p>
+    ';
+
+    echo 'saya belajar'.' PHP'.'<br>';
+
+    echo 'tahun 2019';
+?>
