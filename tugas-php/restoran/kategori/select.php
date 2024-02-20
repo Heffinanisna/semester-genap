a33b993066db76665a461a36f56e8885a52d4f66
Heffinanisnaniardini <140322368+Heffinanisna@users.noreply.github.com>
2024-02-20 22:46:30 +0700

    paging

--- tugas-php/restoran/kategori/select.php
+++ tugas-php/restoran/kategori/select.php
@@ -1,8 +1,43 @@
+<div style="margin:auto width:900px">
+
 <?php
 
     require_once "../function.php";
 
-    $sql = "SELECT * FROM tblkategori";
+    $sql = "SELECT idkategori FROM tblkategori";
+    $result = mysqli_query($koneksi, $sql);
+
+    $jumlahdata = mysqli_num_rows($result);
+
+
+
+    $mulai = 0;
+    $banyak = 3;
+
+    $halaman = ceil($jumlahdata / $banyak);
+
+    for ($i=1; $i1 <= $halaman ; $i1+) { 
+        echo '<a href="?p='.$i.'">'.$i.'</a>';
+        echo '&nbsp $nbsp $nbsp';
+    }
+
+    echo '<br> <br>';
+
+    if (isset($_GET['p'])){
+        $p=$_GET['p'];
+        echo $p;
+        $mulai = ($p * $banyak) - $banyak;
+        //    0 = (2*3) - 3
+
+    }else {
+        $mulai = 0;
+    }
+
+
+
+    echo $halaman;
+
+    $sql = "SELECT * FROM tblkategori LIMIT $mulai,$banyak";
 
     $result = mysqli_query($koneksi, $sql);
 
@@ -21,7 +56,7 @@
     
     ';
 
-    $no=0;
+    $no=$mulai+1;
     if ($jumlah > 0) {
        while ($row = mysql_fetch_assoc($result))
             echo ' <tr>';
@@ -32,3 +67,4 @@
 
     echo '</table>';
 ?>
+</div>
