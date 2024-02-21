4c82adfad47645cafc4a8764ee3256bb14833fd2
Heffinanisnaniardini <140322368+Heffinanisna@users.noreply.github.com>
2024-02-21 22:47:11 +0700

    menambah data user

---
+++ tugas-php/restoran/user/inser.php
@@ -0,0 +1,64 @@
+<h3>insert User</h3>
+<div class="form-group">
+    <form action="" method="post">
+        <div class="form-group w-50">
+            <label for="">Nama user</label>
+            <input type="text" name="user" required placeholder="isi user" class="form-control">
+
+        </div>
+        <div class="form-group w-50">
+            <label for="">Email</label>
+            <input type="email" name="email" required placeholder="email" class="form-control">
+
+        </div>
+        <div class="form-group w-50">
+            <label for="">Password</label>
+            <input type="password" name="password" required placeholder="password" class="form-control">
+
+        </div>
+        <div class="form-group w-50">
+            <label for="">Konfirmasi Password</label>
+            <input type="password" name="konfirmasi" required placeholder="password" class="form-control">
+
+        </div>
+        <div class="form-group w-50">
+            <label for="">Level</label><br>
+            <select name="level" id="">
+
+                <option value="admin">admin</option>
+                <option value="kosi">koki</option>
+                <option value="kasir">kasirS</option>
+
+            </select>
+
+        </div>
+        <div>
+
+            <input type="submit" name="simpan" value="simpan" class="btn btn-primary">
+
+        </div>
+     </form>
+</div>
+
+<?php
+
+    if (isset($_POST['simpan'])) {
+       $user = $_POST['user'];
+       $email = $_POST['email'];
+       $password = $_POST['password'];
+       $konfirmasi = $_POST['konfirmasi'];
+       $level = $_POST['level'];
+
+       if ($password === $konfirmasi) {
+            $sql = "INSERT INTO tbluser VALUES ('','$user','$email','$password','$level',1)";
+            echo $sql;
+            $db->runSQL($sql);
+            header("Location:?f=user&m=.select");
+       }else {
+            echo = "<h2>PASSWORD TIDAK SAMA DENGAN KONFIRMASI</h2>";
+       }
+
+
+    }
+
+?>
