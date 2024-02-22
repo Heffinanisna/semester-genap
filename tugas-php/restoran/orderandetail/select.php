cfff9b4b78a06d44055672b59a0e3c339a5e22e8
Heffinanisnaniardini <140322368+Heffinanisna@users.noreply.github.com>
2024-02-22 23:30:26 +0700

    detail pembelian

--- tugas-php/restoran/orderandetail/select.php
+++ tugas-php/restoran/orderandetail/select.php
@@ -1,7 +1,37 @@
+<h1>Detail Pemebelian</h1>
+
+<div class="form-group">
+    <form action="" method="post">
+        <div class="form-group w-50">
+            <label for="">Tanggan Awal</label>
+            <input type="date" name="tawal" required  class="form-control">
+
+        </div>
+
+        <div class="form-group w-50">
+            <label for="">Tanggan Akhir</label>
+            <input type="date" name="takhir" required  class="form-control">
+
+        </div>
+        <div>
+
+            <input type="submit" name="simpan" value="cari" class="btn btn-primary">
+
+        </div>
+     </form>
+</div>
+
 <?php
 
+    if (isset($_POST['simpan'])) {
+        $tawal = $_POST['tawal'];
+        $takhir = $_POST['takhir'];
+        $sql = "SELECT * FROM vorderdetail WHERE tglorder BETWEEN '$tawal' AND '$takhir'";
+        echo $sql;
+    }
+
     $email=$_SESSION['pelanggan'];
-    $jumlahdata = $db->rowCOUNT("SELECT idorder FROM vorder");
+    $jumlahdata = $db->rowCOUNT("SELECT idorderdetail FROM vorderdetail ");
     $banyak = 4;
 
     $halaman = ceil($jumlahdata / $banyak);
@@ -17,57 +47,69 @@
 
 
 
-    $sql = "SELECT * FROM vorder  ORDER BY status, idorder ASC LIMIT $mulai,$banyak";
+    $sql = "SELECT * FROM vorderdetail ORDER BY idorderdetail DESC LIMIT $mulai,$banyak";
     $row = $db->getALL($sql;)
 
     $no=1=$mulai;
 
+    $total=0;
+
 ?>
 
 
-
-<h1>Order Pemebelian</h1>
-<table class="table table-bordered w-40">
-    <thead>
+<table class="table table-bordered w-70 mt-40">
+    <thead> 
         <tr>
             <th>No</th>
-            <th>Pelanggan</th>
             <th>Tanggal</th>
+            <th>Menu</th>
+            <th>Harga</th>
+            <th>Jumlah</th>
             <th>Total</th>
-            <th>Bayar</th>
-            <th>Kembali</th>
-            <th>Status</th>
+            <th>Pelanggan</th>
+            <th>Alamat</th>
         </tr>
     </thead>
     <tbody>
     <?php if(!empty($row)) {?>
         <?php foreach($row as $r): ?>
-            <?php
-                
-                    if ($r['status']==0) {
-                        $status=' <td><a href="?f=order&m=bayarl&id='.$r['idorder'].'">Bayar</a></td>';
-                    } else{
-                        $status= '<td>LUNAS</td>';
-                    }
-                
-            ?>
+           
             <tr>
                 <td><?php echo $no++?></td>
                 <td><?php echo $r['pelanggan']?></td>
                 <td><?php echo $r['tglorder']?></td>
+                <td><?php echo $r['menu']?></td>
+                <td><?php echo $r['harga']?></td>
+                <td><?php echo $r['jumlah']?></td>
                 <td><?php echo $r['total']?></td>
-                <td><?php echo $r['bayar']?></td>
-                <td><?php echo $r['kembali']?></td>
-                <?php echo $status;?>
+                <td><?php echo $r['alamat']?></td>
+
+                <?php
+                
+                    $total = $total+ ($r['jumlah'] * $r['harga']);
+                
+                ?>
+                
             </tr>
         <?php endforeach ?>
         <?php }?>
+
+        <tr>
+
+            <td colspan="6"><h3>Grand Total</h3></td>
+
+            <td><h4><?php echo $total;?></h4></td>
+
+
+        </tr>
+        
+
     </tbody>
 </table>
 <?php
 
     for ($i=1; $i1 <= $halaman ; $i1+) { 
-        echo '<a href="?f=order&m=select&p'.$i.'">'.$i.'</a>';
+        echo '<a href="?f=detail&m=select&p'.$i.'">'.$i.'</a>';
         echo '&nbsp $nbsp $nbsp';
     }
 
